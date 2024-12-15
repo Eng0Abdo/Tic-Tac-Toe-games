fa8dacf9bc06779183bb890aae85b29eb284e95c
mennamohammed1234 <menna123momo@gmail.com>
2024-12-15 19:57:25 +0200

    new game2

--- Task2/CONNECT_FOUR_H.h
+++ Task2/CONNECT_FOUR_H.h
@@ -22,16 +22,16 @@ public:
 };
 
 template <typename T>
-class human_player : public Player<T> {
+class CONNECT4_human_player : public Player<T> {
 public:
- human_player(string name, T symbol);
+ CONNECT4_human_player(string name, T symbol);
  void getmove(int& x, int& y) override;
 };
 
 template <typename T>
-class random_player : public RandomPlayer<T> {
+class CONNECT4_random_player : public RandomPlayer<T> {
 public:
-    random_player(string name, T symbol);
+    CONNECT4_random_player(string name, T symbol);
     void getmove(int& x, int& y) override;
 };
 
@@ -163,10 +163,10 @@ bool CONNECT4_BOARD<T>::game_is_over() {
 
 // Implementation of player1
 template <typename T>
-human_player<T>::human_player(string name, T symbol) : Player<T>(name, symbol) {}
+CONNECT4_human_player<T>::CONNECT4_human_player(string name, T symbol) : Player<T>(name, symbol) {}
 
 template <typename T>
-void human_player<T>::getmove(int& x, int& y) {
+void CONNECT4_human_player<T>::getmove(int& x, int& y) {
     do {
         cout << this->name << ", enter column (0-6): ";
         cin >> y;
@@ -179,15 +179,119 @@ void human_player<T>::getmove(int& x, int& y) {
 
 // Implementation of player2
 template <typename T>
-random_player<T>::random_player(string name, T symbol) : RandomPlayer<T>(symbol) {
+CONNECT4_random_player<T>::CONNECT4_random_player(string name, T symbol) : RandomPlayer<T>(symbol) {
     this->name = name+ "(Random Player)";
     srand(static_cast<unsigned int>(time(0)));
 }
 
 template <typename T>
-void random_player<T>::getmove(int& x, int& y) {
+void CONNECT4_random_player<T>::getmove(int& x, int& y) {
     y = rand() % 7;  // set random column
     x = 9; // not used
 }
 
 #endif
+
+
+
+
+// main
+//void game2() {
+
+   // while (true)
+   //  {
+    //    string choice;
+      //  Player<char>* players[2];
+       // CONNECT4_BOARD<char>* theBoard = new CONNECT4_BOARD<char>();
+       // string player1Name, player2Name;
+
+       // cout << "Welcome to CONNECT 4 Game." << endl;
+
+      //  cout << "Enter Player 1 name: ";
+       // cin >> player1Name;
+
+
+       // while (true)
+        //{
+         //   cout << "Choose Player 1 type:\n";
+          //  cout << "1. Human\n";
+          //  cout << "2. Random Computer\n";
+           // cin >> choice;
+
+         //   if (choice == "1") {
+          //      players[0] = new CONNECT4_human_player<char>(player1Name, 'X');
+           //     break;
+           // }
+           // else if (choice == "2") {
+            //    players[0] = new CONNECT4_random_player<char>(player1Name,'X');
+              //  break;
+           // }
+          //  else {
+               // cout << "please enter a valid choice" << endl;
+          //  }
+
+       // }
+
+
+       // cout << "Enter Player 2 name: ";
+       // cin >> player2Name;
+
+       // while (true)
+       // {
+         //   cout << "Choose Player 2 type:\n";
+          //  cout << "1. Human\n";
+           // cout << "2. Random Computer\n";
+          //  cin >> choice;
+
+          //  if (choice == "1") {
+           //     players[1] = new CONNECT4_human_player<char>(player2Name, 'O');
+            //    break;
+          //  }
+           // else if (choice == "2") {
+            //    players[1] = new CONNECT4_random_player<char>(player2Name,'O');
+             //   break;
+           // }
+           // else {
+           //     cout << "please enter a valid choice" << endl;
+           // }
+
+        //}
+
+        //GameManager<char> connect4game(theBoard, players);
+        //connect4game.run();
+
+       // delete theBoard;
+       // for (int i = 0; i < 2; ++i) {
+        //    delete players[i];
+      //  }
+
+       // cout << "Your game is finished do you want to continue ?" << endl;
+       // cout << "[1] YES" << endl;
+       // cout << "[2] NO" << endl;
+       // string exit;
+        //while (true)
+       // {
+         //   cin >> exit;
+         //   if (exit == "1") {
+          //      break;
+           // }
+          //  else if (exit == "2") {
+          //      break;
+         //   }
+         //   else {
+            //    cout << "please enter a valid option" << endl;
+              //  cout << "[1] YES" << endl;
+              //  cout << "[2] NO" << endl;
+           // }
+
+       // }
+
+       // if (exit == "1") {
+         //   continue;
+       // }
+       // else if (exit == "2") {
+       //     break;
+      //  }
+   // }
+
+//}
